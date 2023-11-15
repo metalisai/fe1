358e82a7ec6d1720ce010a0745a83b6ef920af76
Talis Tammearu <talis995@gmail.com>
2023-11-16 00:18:25 +0200

    blargh

--- app/page.tsx
+++ app/page.tsx
@@ -3,93 +3,76 @@ import styles from './page.module.css'
 
 export default function Home() {
   return (
+    <div className={styles.container}>
     <main className={styles.main}>
-      <div className={styles.description}>
-        <p>
-          Get started by editing&nbsp;
-          <code className={styles.code}>app/page.tsx</code>
-        </p>
-        <div>
-          <a
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{' '}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className={styles.vercelLogo}
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
+      <div className={styles.center}>
+        <div className={styles.result}>
+            <h2>Your Result</h2>
+            <div className={styles.resultNumber}>
+                <h3>75</h3>
+                <p>of 100</p>
+            </div>
+            <div className={styles.resultText}>
+              <h3>Great</h3>
+              <p>You scored higher than 65% of the people who have taken these tests.</p>
+            </div>
+        </div>
+        <div className={styles.sumcont}>
+          <h2>Summary</h2>
+          <div className={styles.summary}>
+            <div className={`${styles.sumitem} ${styles.textReaction}`}>
+              <span className={styles.citem}><Image
+                src="/icon-reaction.svg"
+                alt="Reaction Icon"
+                className={styles.texticon}
+                width={20}
+                height={20}
+                priority
+              />
+              Reaction</span>
+              <span className={styles.precc}><span className={styles.precc2}>76</span> / 100</span>
+            </div>
+            <div className={`${styles.sumitem} ${styles.textMemory}`}>
+              <span className={styles.citem}><Image
+                src="/icon-memory.svg"
+                alt="Reaction Icon"
+                className={styles.texticon}
+                width={20}
+                height={20}
+                priority
+              />
+              Memory</span>
+              <span className={styles.precc}><span className={styles.precc2}>74</span> / 100</span>
+            </div>
+            <div className={`${styles.sumitem} ${styles.textVerbal}`}>
+              <span className={styles.citem}><Image
+                src="/icon-verbal.svg"
+                alt="Reaction Icon"
+                className={styles.texticon}
+                width={20}
+                height={20}
+                priority
+              />
+              Verbal</span>
+              <span className={styles.precc}><span className={styles.precc2}>78</span> / 100</span>
+            </div>
+            <div className={`${styles.sumitem} ${styles.textVisual}`}>
+              <span className={styles.citem}><Image
+                src="/icon-visual.svg"
+                alt="Reaction Icon"
+                className={styles.texticon}
+                width={20}
+                height={20}
+                priority
+              />
+              Visual</span>
+              <span className={styles.precc}><span className={styles.precc2}>72</span> / 100</span>
+            </div>
+            <button className={styles.btn}>Continue</button>
+          </div>
         </div>
       </div>
-
-      <div className={styles.center}>
-        <Image
-          className={styles.logo}
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
-
-      <div className={styles.grid}>
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Docs <span>-&gt;</span>
-          </h2>
-          <p>Find in-depth information about Next.js features and API.</p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Learn <span>-&gt;</span>
-          </h2>
-          <p>Learn about Next.js in an interactive course with&nbsp;quizzes!</p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Templates <span>-&gt;</span>
-          </h2>
-          <p>Explore starter templates for Next.js.</p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Deploy <span>-&gt;</span>
-          </h2>
-          <p>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
     </main>
+    </div>
   )
 }
